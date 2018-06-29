8b746bfab1fcd6b484975e1d20e519d7a80cbf43
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-06-29 16:58:30 -0700

    finish phase 4 of my_hash_map

--- my_hash_map/skeleton/lib/p04_linked_list.rb
+++ my_hash_map/skeleton/lib/p04_linked_list.rb
@@ -19,7 +19,13 @@ class Node
 end
 
 class LinkedList
+  include Enumerable 
+  
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -28,30 +34,67 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    self.each do |node| 
+      return node.val if node.key == key 
+    end 
+    nil 
   end
 
   def include?(key)
+    self.each do |node| 
+      return true if node.key == key 
+    end 
+    false
+    # current_node = first 
+    # while current_node.key != key 
+    #   current_node = first.next 
+    # end 
   end
 
   def append(key, val)
+    new_node = Node.new(key, val)
+    new_node.prev = last
+    last.next = new_node
+    new_node.next = @tail
+    @tail.prev = new_node
   end
 
   def update(key, val)
+    self.each do |node|
+      node.val = val if node.key == key
+      
+    end
   end
 
   def remove(key)
+    self.each do |node|
+      if node.key == key
+        prev_node = node.prev
+        prev_node.next = node.next
+        node.next.prev = prev_node
+      end
+      
+    end
   end
 
-  def each
+  def each(&prc)
+    current_node = first 
+    until current_node == @tail
+      prc.call(current_node)
+      current_node = current_node.next
+    end 
   end
 
   # uncomment when you have `each` working and `Enumerable` included
